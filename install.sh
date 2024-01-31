0cfdb24f52da86ba5bd2e423bb7c3cdb4f022e3c
davidht <davidht@gmail.com>
2024-01-31 07:52:11 +0100

    Clean install

--- install.sh
+++ install.sh
@@ -17,46 +17,41 @@ echo
 
 read -p "Do you like continue with the installation [y/N]? " yesNoOption
 
-if [ $yesNoOption -eq "y" ] 
-then
-    exit 0
+if [ "$yesNoOption" = "y" ]; then
+    echo "-------------------------- Update apt"
+    sudo apt update
+
+    echo "-------------------------- Install g++"
+    sudo apt install g++
+
+    echo "-------------------------- Install Make"
+    sudo apt install make
+
+    echo "-------------------------- Install CMake"
+    sudo apt install cmake
+
+    echo "-------------------------- Install Boost"
+    sudo apt install libboost-all-dev
+
+    echo "-------------------------- Install JSon"
+    sudo apt-get install libjsoncpp-dev
+
+    echo "-------------------------- Install LibRdKafka"
+    sudo apt-get install -y librdkafka-dev
+
+    echo "-------------------------- Install MySQL Connector"
+    sudo apt-get install libmysqlcppconn-dev
+
+    echo "-------------------------- Download, build & Install CppKafka"
+    sudo apt install libssl-dev
+    cd ..
+    git clone https://github.com/mfontanini/cppkafka.git
+    cd cppkafka
+    mkdir build
+    cd build
+    cmake ..
+    make
+    sudo cp src/lib/libcppkafka.so /usr/lib
+    cd ../..
+    cd middlenebel-server
 fi
-
-echo "-------------------------- Update apt"
-sudo apt update
-
-echo "-------------------------- Install g++"
-sudo apt install g++
-
-echo "-------------------------- Install Make"
-sudo apt install make
-
-echo "-------------------------- Install CMake"
-sudo apt install cmake
-
-echo "-------------------------- Install Boost"
-sudo apt install libboost-all-dev
-
-echo "-------------------------- Install JSon"
-sudo apt-get install libjsoncpp-dev
-
-echo "-------------------------- Install LibRdKafka"
-sudo apt-get install -y librdkafka-dev
-
-echo "-------------------------- Install MySQL Connector"
-sudo apt-get install libmysqlcppconn-dev
-
-echo "-------------------------- Download, build & Install CppKafka"
-sudo apt install libssl-dev
-cd ..
-git clone https://github.com/mfontanini/cppkafka.git
-cd cppkafka
-mkdir build
-cd build
-cmake ..
-make
-sudo cp src/lib/libcppkafka.so /usr/lib
-cd ../..
-cd middlenebel-server
-
-
